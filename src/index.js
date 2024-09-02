bcaf86185e9118f1fd177c1e9c56ce3480e0eee4
Dmitry Shirkov <dimashirkov01051995@gmail.com>
2024-09-02 15:23:10 +0300

    feat: helpers.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/style.css';
+import './scss/helpers.scss'
 import './scss/mixin.scss';
 import './scss/fonts.scss';
 import './scss/style.scss'; 
+import 'swiper/scss';
 import './js/app';
 
 import './scss/main.scss';
